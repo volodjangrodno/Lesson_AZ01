de213633c35011a3fa6a47ab7727eb91e5ff2c9b
volodjangrodno <parthnersclub.ru@gmail.com>
2024-06-03 11:25:44 +0300

    add to AZ01dz1.py

--- AZ01dz2.py
+++ AZ01dz2.py
@@ -2,7 +2,9 @@ import pandas as pd
 
 
 df = pd.read_csv("dz.csv")
-print(df)
+print(df.head) # выводим первые 5 строк
+# print(df.tail) # выводим последние 5 строк
+# print(30) # выводим строку с индексом 30
 
 df.fillna(0, inplace=True) # заполнение пропусков нулями
 print(df)
